6c119de5ce2a29b31dacdc3b2f1e7929937507b5
Dom <Dom@localhost>
2017-08-18 13:05:04 +1200

    Comment added to controller class

--- AppointmentRepo2/SoftwareDevelopment/src/sourceCode/Controller.java
+++ AppointmentRepo2/SoftwareDevelopment/src/sourceCode/Controller.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Vector;
 
+// Here is the  code that is used for running the Database
 
 	public class Controller {
  	private User u;
